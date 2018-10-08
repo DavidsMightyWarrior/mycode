c92d5fb74bbbec7c2de78b1a6c8d861284ec197e
Erik Waldrop <erik@davidsmightywarrior.com>
2018-10-08 13:21:31 -0600

    New Print List

---
+++ mix-list/mixList.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+my_list=["192.168.0.5", 5060, "UP"]
+print("The first item in list (IP): "+my_list[0])
+print("The second item in list (port): "+str(my_list[1]))
+print("The third item in list (state): "+my_list[2])
