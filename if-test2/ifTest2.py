a4704a3e7beaec0f51ef05ac0e955b9ab0f20f89
Erik Waldrop <erik@davidsmightywarrior.com>
2018-10-08 15:14:58 -0600

    If Else Loops

---
+++ if-test2/ifTest2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+ipchk = input('Apply an IP address: ')
+
+if ipchk:
+    print('Looks like the IP address was set: ' + ipchk)
+
+else: 
+    print('You did not provide input.')
