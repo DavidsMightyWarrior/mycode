0476f3db8af1fbf92514d89e18db6c1b38174b63
Erik Waldrop <erik@davidsmightywarrior.com>
2018-10-08 13:03:51 -0600

    Added comments

---
+++ input_ip.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Written By: Erik Waldrop
+# Collecti IP address information from user, and display it back to them.
+
+## Collect input from the user
+user_input = input('Please enter an IPv4 IP address:')
+
+## Print out the input collected from the user
+print("You told me the IPv4 address is:" + user_input)
